42a516874b1e335b608883e24f985736e9316c31
ThibaudL <lamarchethibaud@hotmail.fr>
2014-05-26 09:42:08 +0200

    Added launch script to copy the lib and launch opp(you to need to modify paths) Modified link to plugin.h

---
+++ OCPM/launch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp -R -f "/home/thibaud/COMPILATION QT/ocpm_plugin DEBUG/pluginsOPP" "/home/thibaud/opp-build-desktop-Qt_4_8_1_dans_le_PATH__Syst_me__Debug/"
+/home/thibaud/opp-build-desktop-Qt_4_8_1_dans_le_PATH__Syst_me__Debug/opp
